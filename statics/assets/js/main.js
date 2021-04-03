92a026666ee09486278d248e3afa6897d34a47a3
tranngoc769 <tranngoc769@gmail.com>
2021-04-03 11:54:44 +0700

    finish

--- statics/assets/js/main.js
+++ statics/assets/js/main.js
@@ -10,6 +10,25 @@
             $("#caption_insta").text($caption);
             $("#insta_img")[0].src = $src;
             $("#insta_img2")[0].src = $src;
+            $("#download_insta").attr("src", $src)
+        });
+        $(document).on('click', "a[id='download_insta']", function() {
+            $src = $(this).attr('src');
+            console.log($src);
+            var today = new Date();
+            var date = today.getFullYear() + '_' + (today.getMonth() + 1) + '_' + today.getDate();
+            var time = today.getHours() + "_" + today.getMinutes() + "_" + today.getSeconds();
+            var dateTime = date + '_' + time;
+            // 
+            var link = document.createElement('a');
+            link.href = $src;
+            link.download = 'Download.jpg';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+
+
+
         });
     });
 
